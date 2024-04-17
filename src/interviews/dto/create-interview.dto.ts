8186d066446952fb1f788b6fd93e00a1acdacad8
Майоров Артем Игоревич <artmai.pg@outlook.com>
2024-04-17 21:01:22 +0300

    добавил описание для свагера

--- src/interviews/dto/create-interview.dto.ts
+++ src/interviews/dto/create-interview.dto.ts
@@ -1,11 +1,22 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CreateInterviewDto {
+   @ApiProperty({ example: 'Газпром', description: 'Название компании' })
    readonly companyName: string;
+   @ApiProperty({ example: 'successful', description: 'Финальный статус взаимодействия с компанией' })
    readonly status: string;
+   @ApiProperty({ example: 'middle,senior,junior', description: 'Грейд требуемый компанией. Может быть несколько значений' })
    readonly grade: string;
+   @ApiProperty({ example: 'livecoding,conversation,techpart', description: 'Этап взаимодействия с компанией' })
    readonly stage: string;
+   @ApiProperty({ example: 'Моё собеседование в газпром, это было ужасно', description: 'Заголовок статьи' })
    readonly articleTitle: string;
+   @ApiProperty({ example: 'Я ему одно, он мне другое, так и живём', description: 'Описание статьи, будет формате markdown' })
    readonly articleDescription: string;
+   @ApiProperty({ example: 'Лютый', description: 'Уникальный никнэйм пользователя' })
    readonly nickName: string
+   @ApiProperty({ example: 10000, description: 'Минимальное предложение по зп. Может быть не указано' })
    readonly minoffer: number
+   @ApiProperty({ example: 20000, description: 'Максимальное предложение по зп. Может быть не указано' })
    readonly maxoffer: number
 }
--- src/interviews/interviews.model.ts
+++ src/interviews/interviews.model.ts
@@ -1,7 +1,8 @@
 import { AllowNull, BelongsTo, Column, DataType, ForeignKey, Model, Table} from "sequelize-typescript";
 import { User } from "src/users/users.model";
+import { ApiTags, ApiProperty } from '@nestjs/swagger';
 
-// Шаг 1 - описываем модель для создания интерьвью
+
 
 interface InterviewCreatorAttrs {
     companyName: string;
@@ -15,39 +16,48 @@ interface InterviewCreatorAttrs {
     minoffer: number
     prefix: string
 }
-
+@ApiTags('interviews')
 @Table({tableName: 'interviews', timestamps: true})
 export class Interview extends Model<Interview, InterviewCreatorAttrs> {
+    @ApiProperty({example: 1, description: 'Уникальный идентификатор'})
     @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
     id: number;
-
+    @ApiProperty({example: 'Газпром', description: 'Название компании'})
     @Column({type: DataType.STRING, allowNull: false})
     companyName: string;
 
+    @ApiProperty({example: 'Супер собеседование', description: 'Заголовок названия собеседования'})
     @Column({type: DataType.STRING, allowNull: false})
     articleTitle: string;
-    
+
+    @ApiProperty({example: 'Это было волшебно', description: 'Описание названия собеседования'})
     @Column({type: DataType.STRING, allowNull: false})
     articleDescription: string;
 
+    @ApiProperty({ example: 'successful', description: 'Финальный статус взаимодействия с компанией' })
     @Column({type: DataType.STRING, allowNull: false, unique: false, autoIncrement: false, primaryKey: true})
     status: string;
 
+    @ApiProperty({ example: 'middle,senior,junior', description: 'Грейд требуемый компанией. Может быть несколько значений' })
     @Column({type: DataType.STRING, allowNull: false, unique: false,autoIncrement: false, primaryKey: true})
     grade: string;
     
+    @ApiProperty({ example: 'livecoding,conversation,techpart', description: 'Этап взаимодействия с компанией' })
     @Column({type: DataType.STRING, allowNull: false, unique: false,autoIncrement: false, primaryKey: true})
     stage: string;
     
+    @ApiProperty({ example: 'Лютый', description: 'Уникальный никнэйм пользователя' })
     @Column({type: DataType.STRING, allowNull: true, unique: false})
     nickName: string;
 
+    @ApiProperty({ example: 20000, description: 'Максимальное предложение по зп. Может быть не указано' })
     @Column({type: DataType.INTEGER, allowNull: true})
     maxoffer : number;
 
+    @ApiProperty({ example: 10000, description: 'Минимальное предложение по зп. Может быть не указано' })
     @Column({type: DataType.INTEGER, allowNull: true})
     minoffer : number;
-
+    @ApiProperty({ example:'от', description: 'Префикс для правильного вывода оффера, необязательное' })
     @Column({type: DataType.STRING, allowNull: true})
     prefix: string; 
 }
