4c1b084550007499a8aedc851178dee8797f53b3
Майоров Артем Игоревич <artmai.pg@outlook.com>
2024-04-18 11:13:41 +0300

    'fix/getDTO'

--- src/interviews/interviews.controller.ts
+++ src/interviews/interviews.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Get, Headers, Post, Query } from "@nestjs/common";
 import { ApiOperation, ApiQuery, ApiResponse, ApiTags } from "@nestjs/swagger";
 import { CreateInterviewDto } from "src/interviews/dto/create-interview.dto";
+import { InterviewsPaginationDto } from "src/interviews/dto/interview-pagination.dto";
 import { Interview } from "src/interviews/interviews.model";
 import { InterviewsService } from "src/interviews/interviews.service";
 @ApiTags('interviews')
@@ -18,7 +19,7 @@ export class InterviewsController {
 
   @Get()
   @ApiOperation({ summary: 'Получение и фильтрация статей'  })
-  @ApiResponse({ status: 200, description: 'Получить и отфильтровать список', type:  [Interview] })
+  @ApiResponse({ status: 200, description: 'Получить и отфильтровать список', type:  InterviewsPaginationDto })
   @ApiQuery({ name: 'status', required: false, description: 'Фильтрация по статусу', type: String })
   @ApiQuery({ name: 'stage', required: false, description: 'Фильтрация по этапу', type: String })
   @ApiQuery({ name: 'grade', required: false, description: 'Фильтрация по грейду', type: String })
