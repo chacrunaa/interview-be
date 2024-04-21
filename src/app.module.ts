39103776282c5ca27aa4879dc430532348a67b77
unknown <andreyandreydn16@gmail.com>
2024-04-21 15:21:13 +0300

    delete letter s

--- src/app.module.ts
+++ src/app.module.ts
@@ -43,9 +43,9 @@ import { ResponseInterceptor } from "src/common/interseptors/response.intercepto
     SequelizeModule.forRoot({
       dialect: "postgres",
       host: process.env.POSTGRES_HOST,
-      port: Number(process.env.POSTGRESS_PORT),
+      port: Number(process.env.POSTGRES_PORT),
       username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRESS_PASSWORD,
+      password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DB,
       models: [User, Role, UserRoles, Post],
       autoLoadModels: true,
